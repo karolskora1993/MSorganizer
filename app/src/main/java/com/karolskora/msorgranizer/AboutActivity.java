fd933b4d01729b634704cbf5e313268795c82b36
karolskora1993 <karol.skora@icloud.com>
2016-03-05 10:48:34 +0100

    ekran początkowy i ekran z informacjami o leku

---
+++ app/src/main/java/com/karolskora/msorgranizer/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.karolskora.msorgranizer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_about);
+    }
+
+}
---
+++ app/src/main/java/com/karolskora/msorgranizer/InitialSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.karolskora.msorgranizer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InitialSettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_initial_settings);
+    }
+}
