adc47b4249caa45866457a97a6dc724ba0ff9039
karolskora1993 <karol.skora@icloud.com>
2016-03-08 12:09:11 +0100

    wprowadzenie daty i godziny przypomnień
    -aktywność do wprowadzania daty i godziny
    -fragmenty DatePicker oraz TimePicker

--- app/src/main/java/com/karolskora/msorgranizer/activities/FirstInjectionTimeActivity.java
+++ app/src/main/java/com/karolskora/msorgranizer/activities/FirstInjectionTimeActivity.java
@@ -1,15 +1,25 @@
 package com.karolskora.msorgranizer.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+
 
 import com.karolskora.msorgranizer.R;
+import com.karolskora.msorgranizer.fragments.DatePickerFragment;
 
-public class FirstInjectionTimeActivity extends AppCompatActivity {
+public class FirstInjectionTimeActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_first_injection_time);
+
+    }
+
+    public void onButtonNextClick(View view){
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
     }
 }
